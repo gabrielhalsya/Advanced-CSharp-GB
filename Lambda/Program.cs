4e1e1686b5a1215495794275f2352704ec999d99
gabrielhalsya <gabrielm1603@gmail.com>
2023-01-31 14:45:49 +0700

    add lambda implementation example

---
+++ Lambda/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace Lambda
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Lambda Implementation");
+            int[] list = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+
+            //calling isevenlambda in another class as func parameter
+            //but func parameter must be 1 int param and return bool
+            //cause Count(int[] inp,func<int,bool> func)
+            int totalEven = SampleLambda.Count(list, SampleLambda.IsEvenLambda);
+            Console.WriteLine($"total even : {totalEven}");
+
+            //using lambda inline if one statement
+            int totalEven2 = SampleLambda.Count(list,x=>x%2==0);
+            Console.WriteLine($"total even2 : {totalEven2}");
+
+            //or using lambda with {} if there is more than one statement
+            int totalEven3 = SampleLambda.Count(list, x =>
+            {
+                Console.WriteLine(x);
+                return x%2==0;
+            });
+
+            
+        }
+    }
+}
---
+++ Lambda/SampleLambda.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda
+{
+    internal class SampleLambda
+    {
+        public static bool IsEvenNoLambda(int number)
+        {
+            return number %2 == 0;
+        }
+        
+        //using lambda
+        public static bool IsEvenLambda(int number) => number % 2 == 1;
+
+        public static int Count(int[] inputArray, Func<int, bool> func)
+        {
+            int count = 0;
+            foreach (var input in inputArray)
+            {
+                if(func(input)) count++;
+            }
+            return count;
+        }
+    }
+}
